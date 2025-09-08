b58bf43635b1732299ae0400da3835e8222ef2f4
Joshua Villanova <joshuacvillanova@gmail.com>
2025-09-08 01:06:39 -0700

    begin adding navbar dropdowns

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,61 +3,85 @@ import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { NavigationMenu } from 'radix-ui';
 
 function ContentList() {
     const { basePath } = useRouter();
 
     return (
-        <div id='navbar'>
-            <ul className='left-navbar'>
-                <li className="navbar-logo">
-                    <img
-                        alt="Covina High School's logo"
-                        src={`${basePath}/assets/images/icons/covina-logo.png`}
-                    />
-                </li>
-                <li id='logo-text'>
-                    <button>
-                        <Link href='/'>
-                            <span className='red-text'>COVINA</span>
-                            <span>BAND&GUARD</span>
-                        </Link>
-                    </button>
-                </li>
-            </ul>
-            <div
-                style={{
-                    width: '50rem',
-                }}
-            />
-            <ul className='right-navbar'>
-                <li className='navbar-item'>
-                    <button>
-                        <Link className='right-link' href='/Shows'>Shows</Link>
-                    </button>
-                </li>
-                <li className='navbar-item'>
-                    <button>
-                        <Link className='right-link' href='/Ensembles'>Ensembles</Link>
-                    </button>
-                </li>
-                <li className='navbar-item'>
-                    <button>
-                        <Link className='right-link' href='/Staff'>Staff</Link>
-                    </button>
-                </li>
-                <li className='navbar-item'>
-                    <button>
-                        <Link className='right-link' href='/Schedule'>Schedule</Link>
-                    </button>
-                </li>
-                <li className='navbar-item'>
-                    <button>
-                        <Link className='right-link' href='/GetInvolved'>Get Involved</Link>
-                    </button>
-                </li>
-            </ul>
-        </div>  
+        <NavigationMenu.Root>
+            <NavigationMenu.List id='navbar'>
+                <ul className='left-navbar'>
+                    <li className="navbar-logo">
+                        <img
+                            alt="Covina High School's logo"
+                            src={`${basePath}/assets/images/icons/covina-logo.png`}
+                        />
+                    </li>
+                    <li id='logo-text'>
+                        <button>
+                            <Link href='/'>
+                                <span className='red-text'>COVINA</span>
+                                <span>BAND&GUARD</span>
+                            </Link>
+                        </button>
+                    </li>
+                </ul>
+                <div
+                    style={{
+                        flex: '4 1 auto',
+                    }}
+                />
+                <ul className='right-navbar'>
+                    <NavigationMenu.Item className='navbar-item'>
+                        <NavigationMenu.Trigger asChild>
+                            <button>
+                                <Link className='right-link' href='/Shows'>Shows</Link>
+                            </button>
+                        </NavigationMenu.Trigger>
+                        <NavigationMenu.Content className='navbar-dropdown'>
+                            <ul>
+                                <li className='navbar-subitem'>
+                                    <button>
+                                        <Link className='right-link' href='/Shows/Slither'>Slither</Link>
+                                    </button>
+                                </li>
+                                <li className='navbar-subitem'>
+                                    <button>
+                                        <Link className='right-link' href='/Shows/OnStage'>On Stage</Link>
+                                    </button>
+                                </li>
+                                <li className='navbar-subitem'>
+                                    <button>
+                                        <Link className='right-link' href='/Shows/Rise'>Rise</Link>
+                                    </button>
+                                </li>
+                            </ul>
+                        </NavigationMenu.Content>
+                    </NavigationMenu.Item>
+                    <li className='navbar-item'>
+                        <button>
+                            <Link className='right-link' href='/Ensembles'>Ensembles</Link>
+                        </button>
+                    </li>
+                    <li className='navbar-item'>
+                        <button>
+                            <Link className='right-link' href='/Staff'>Staff</Link>
+                        </button>
+                    </li>
+                    <li className='navbar-item'>
+                        <button>
+                            <Link className='right-link' href='/Schedule'>Schedule</Link>
+                        </button>
+                    </li>
+                    <li className='navbar-item'>
+                        <button>
+                            <Link className='right-link' href='/GetInvolved'>Get Involved</Link>
+                        </button>
+                    </li>
+                </ul>
+            </NavigationMenu.List>
+        </NavigationMenu.Root>  
     );
 }
 
