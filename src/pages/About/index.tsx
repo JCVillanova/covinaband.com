0a8df5d483c0c6e13ca4930021102074568c0ada
Joshua Villanova <joshuacvillanova@gmail.com>
2025-10-27 00:55:49 -0700

    add spacing to about cards

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -14,34 +14,34 @@ export default function About() {
                 <NavBar />
                 <h2>About</h2>
 
-                <Card className='' style={{}}>
-                    <h3>Staff</h3>
-                    <p>
-                        Leading this program requires dedicated people who are invested in our students' success and growth, not just as musicians, but as people. We
-                        have a wonderful team of staff who share this philosophy and are committed to making Covina's band and guard the most enjoyable and fulfilling
-                        program it can be.
-                    </p>
-                    <ClickableButton className='' buttonSize='medium' buttonColor='transparent' buttonText='View staff' style={{}} />
-                </Card>
-
-                <Card className='' style={{}}>
-                    <h3>Boosters</h3>
-                    <p>
-                        Our boosters do incredible work to assist at competitions, support the program, and connect with band families. With many of them being
-                        current band parents, they are vital to our community.
-                    </p>
-                    <ClickableButton className='' buttonSize='medium' buttonColor='transparent' buttonText='View boosters' style={{}} />
-                    <ClickableButton className='' buttonSize='medium' buttonColor='transparent' buttonText='Become a booster' style={{}} />
-                </Card>
-
-                <Card className='' style={{}}>
-                    <h3>Students</h3>
-                    <p>
-                        Covina's band and guard would not be possible without the help of student leadership. These inspiring young leaders work tirelessly to help
-                        their peers achieve their fullest potential and are instrumental to the program's operation.
-                    </p>
-                    <ClickableButton className='' buttonSize='medium' buttonColor='transparent' buttonText='View leaders' style={{}} />
-                </Card>
+                <div className='about-cards'>
+                    <Card className='about-card' style={{}}>
+                        <h3>Staff</h3>
+                        <p>
+                            Leading this program requires dedicated people who are invested in our students' success and growth, not just as musicians, but as people. We
+                            have a wonderful team of staff who share this philosophy and are committed to making Covina's band and guard the most enjoyable and fulfilling
+                            program it can be.
+                        </p>
+                        <ClickableButton className='' buttonSize='medium' buttonColor='transparent' buttonText='View staff' style={{}} />
+                    </Card>
+                    <Card className='about-card' style={{}}>
+                        <h3>Boosters</h3>
+                        <p>
+                            Our boosters do incredible work to assist at competitions, support the program, and connect with band families. With many of them being
+                            current band parents, they are vital to our community.
+                        </p>
+                        <ClickableButton className='' buttonSize='medium' buttonColor='transparent' buttonText='View boosters' style={{}} />
+                        <ClickableButton className='' buttonSize='medium' buttonColor='transparent' buttonText='Become a booster' style={{}} />
+                    </Card>
+                    <Card className='about-card' style={{}}>
+                        <h3>Students</h3>
+                        <p>
+                            Covina's band and guard would not be possible without the help of student leadership. These inspiring young leaders work tirelessly to help
+                            their peers achieve their fullest potential and are instrumental to the program's operation.
+                        </p>
+                        <ClickableButton className='' buttonSize='medium' buttonColor='transparent' buttonText='View leaders' style={{}} />
+                    </Card>
+                </div>
             </div>
             <Footer />
         </div>
