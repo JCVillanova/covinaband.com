a9f3bf05b697280bb0f4026ddc6040988d60b898
Joshua Villanova <joshuacvillanova@gmail.com>
2025-12-20 13:34:14 -0800

    populate About/StudentLeadership

--- src/pages/About/StudentLeadership/index.tsx
+++ src/pages/About/StudentLeadership/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Separator } from 'radix-ui';
 import ClickableButton from '../../../components/Button';
 import NavBar from '../../../components/NavBar';
+import ProfileCard from '../../../components/ProfileCard';
 import Footer from '../../../components/Footer';
 
 // TODO: ADD STUDENT BIOS
@@ -14,6 +15,88 @@ export default function StudentLeadership() {
                 <NavBar />
                 <h2 className='center-text'>STUDENT LEADERSHIP</h2>
                 <Separator.Root className='horizontal-divider' decorative />
+                <div className='profile-cards'>
+                    <ProfileCard
+                        className=''
+                        name='Madilyn Franco'
+                        position='Drum Major'
+                        style={{}}
+                    >
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit. Mus lectus urna tellus auctor quisque egestas fames efficitur. Molestie potenti convallis, rutrum ante semper diam ultricies montes. Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit.
+                    </ProfileCard>
+                    <ProfileCard
+                        className=''
+                        name='Firstname Lastname'
+                        position='Leadership Role'
+                        style={{}}
+                    >
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit. Mus lectus urna tellus auctor quisque egestas fames efficitur. Molestie potenti convallis, rutrum ante semper diam ultricies montes. Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit.
+                    </ProfileCard>
+                    <ProfileCard
+                        className=''
+                        name='Firstname Lastname'
+                        position='Leadership Role'
+                        style={{}}
+                    >
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit. Mus lectus urna tellus auctor quisque egestas fames efficitur. Molestie potenti convallis, rutrum ante semper diam ultricies montes. Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit.
+                    </ProfileCard>
+                    <ProfileCard
+                        className=''
+                        name='Firstname Lastname'
+                        position='Leadership Role'
+                        style={{}}
+                    >
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit. Mus lectus urna tellus auctor quisque egestas fames efficitur. Molestie potenti convallis, rutrum ante semper diam ultricies montes. Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit.
+                    </ProfileCard>
+                    <ProfileCard
+                        className=''
+                        name='Firstname Lastname'
+                        position='Leadership Role'
+                        style={{}}
+                    >
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit. Mus lectus urna tellus auctor quisque egestas fames efficitur. Molestie potenti convallis, rutrum ante semper diam ultricies montes. Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit.
+                    </ProfileCard>
+                    <ProfileCard
+                        className=''
+                        name='Firstname Lastname'
+                        position='Leadership Role'
+                        style={{}}
+                    >
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit. Mus lectus urna tellus auctor quisque egestas fames efficitur. Molestie potenti convallis, rutrum ante semper diam ultricies montes. Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit.
+                    </ProfileCard>
+                    <ProfileCard
+                        className=''
+                        name='Firstname Lastname'
+                        position='Leadership Role'
+                        style={{}}
+                    >
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit. Mus lectus urna tellus auctor quisque egestas fames efficitur. Molestie potenti convallis, rutrum ante semper diam ultricies montes. Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit.
+                    </ProfileCard>
+                    <ProfileCard
+                        className=''
+                        name='Firstname Lastname'
+                        position='Leadership Role'
+                        style={{}}
+                    >
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit. Mus lectus urna tellus auctor quisque egestas fames efficitur. Molestie potenti convallis, rutrum ante semper diam ultricies montes. Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit.
+                    </ProfileCard>
+                    <ProfileCard
+                        className=''
+                        name='Firstname Lastname'
+                        position='Leadership Role'
+                        style={{}}
+                    >
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit. Mus lectus urna tellus auctor quisque egestas fames efficitur. Molestie potenti convallis, rutrum ante semper diam ultricies montes. Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit.
+                    </ProfileCard>
+                    <ProfileCard
+                        className=''
+                        name='Firstname Lastname'
+                        position='Leadership Role'
+                        style={{}}
+                    >
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit. Mus lectus urna tellus auctor quisque egestas fames efficitur. Molestie potenti convallis, rutrum ante semper diam ultricies montes. Lorem ipsum odor amet, consectetuer adipiscing elit. Vel ornare parturient feugiat diam molestie hac. Auctor curae mi platea purus hendrerit.
+                    </ProfileCard>
+                </div>
             </div>
             <Footer />
         </div>
